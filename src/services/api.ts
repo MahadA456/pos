43c66984c3b2957f73ff9e13fdf871af6a90ec19
abdullah2310ishaq <abdullahishaq2310@gmail.com>
2025-09-12 21:54:36 +0500

    phase_1

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://159.65.30.119:8080/api';
-
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://159.65.30.119:8080/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export interface ApiResponse<T = unknown> {
   success: boolean;
   data?: T;
